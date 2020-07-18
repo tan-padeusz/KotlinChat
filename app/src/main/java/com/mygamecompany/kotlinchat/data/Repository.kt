dca00dcbffd08fee3c22d1fb6f0a94d9c9abeb10
Maciek Moryń <moryn.maciej@o2.pl>
2020-07-18 03:04:35 +0200

    Gradle files updated.

--- app/src/main/java/com/mygamecompany/kotlinchat/data/Repository.kt
+++ app/src/main/java/com/mygamecompany/kotlinchat/data/Repository.kt
@@ -2,6 +2,7 @@ package com.mygamecompany.kotlinchat.data
 
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
+import android.net.Uri
 import android.os.ParcelUuid
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -13,14 +14,22 @@ import timber.log.Timber
 
 object Repository : ChatDevice {
 
+    //VALUES
+    private val roomListLiveData: MutableLiveData<List<ChatRoom>> = MutableLiveData()
+
+    //VARIABLES
     var isServer: Boolean = false
     var username: String = ""
-
     private var client: Client? = null
     private var server: Server? = null
 
-    private val roomList: ArrayList<ChatRoom> = ArrayList()
-    private val roomListLiveData: MutableLiveData<List<ChatRoom>> = MutableLiveData()
+    //FUNCTIONS
+    override fun getLastMessage(): LiveData<String> = if (isServer) server!!.getLastMessage() else client!!.getLastMessage()
+    override fun getLastConnectionMessage(): LiveData<String> = if (isServer) server!!.getLastConnectionMessage() else client!!.getLastConnectionMessage()
+    override fun getLastImageUri(): LiveData<Uri> = if (isServer) server!!.getLastImageUri() else client!!.getLastImageUri()
+    override fun sendMessage(message: String) = if (isServer) server!!.sendMessage(message) else client!!.sendMessage(message)
+    override fun sendConnectionMessage(connected: Boolean) = if (isServer) server!!.sendConnectionMessage(connected) else client!!.sendConnectionMessage(connected)
+    fun getChatRoomList(): LiveData<List<ChatRoom>> = roomListLiveData
 
     fun initializeBluetoothDevices(bluetoothAdapter: BluetoothAdapter, context: Context) {
         client = Client(bluetoothAdapter, context)
@@ -32,28 +41,19 @@ object Repository : ChatDevice {
         else client!!.runBluetoothDevice(run)
     }
 
-    override fun sendMessage(message: String) {
-        if (isServer) server!!.sendMessage(message)
-        else client!!.sendMessage(message)
-    }
-
-    override fun getLastMessage(): LiveData<String> {
-        return if (isServer) server!!.getLastMessage()
-        else client!!.getLastMessage()
-    }
-
     fun observeScanResult() {
         if(isServer) {
             Timber.d("Could not observe scan result. Device is server.")
             return
         }
-        client!!.getLastScanResult().observeForever {
-            val address: String = it.device.address
-            val name: String = it.scanRecord?.serviceData?.get(ParcelUuid(Constants.SERVICE_UUID))!!.toString(Charsets.UTF_8)
-            roomList.add(ChatRoom(address, name))
-            roomListLiveData.postValue(roomList)
+        client!!.getFoundChatRooms().observeForever {
+            val roomList: ArrayList<ChatRoom> = ArrayList()
+            for (result in it) {
+                val address: String = result.device.address
+                val name: String = result.scanRecord?.serviceData?.get(ParcelUuid(Constants.SERVICE_UUID))!!.toString(Charsets.UTF_8)
+                roomList.add(ChatRoom(address, name))
+                roomListLiveData.postValue(roomList)
+            }
         }
     }
-
-    fun getChatRoomList(): LiveData<List<ChatRoom>> = roomListLiveData
 }
